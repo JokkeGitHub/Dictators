a2821c61603d534e84c25ddbb4940406e4205b7c
Joachim Hansen <skraldespand94@hotmail.com>
2022-01-27 09:39:22 +0100

    dictators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DictatorsComponent } from './dictators/dictators.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DictatorsComponent
   ],
   imports: [
     BrowserModule,
