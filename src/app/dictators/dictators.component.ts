7b6b8658404f8d7a8638c65981de290efbd3c3cf
Joachim Hansen <skraldespand94@hotmail.com>
2022-02-10 09:08:49 +0100

    Getting started

--- src/app/dictators/dictators.component.ts
+++ src/app/dictators/dictators.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Dictator } from './classes/dictator';
 
 @Component({
   selector: 'app-dictators',
   templateUrl: './dictators.component.html',
   styleUrls: ['./dictators.component.css']
 })
+
 export class DictatorsComponent implements OnInit {
 
+  dictator: Dictator[] = [];
+  
   constructor() { }
 
   ngOnInit(): void {
