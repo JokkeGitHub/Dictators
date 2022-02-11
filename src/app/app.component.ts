a65e7adad679fbbfda9b3e6ab9745b8711e82f16
Joachim Hansen <skraldespand94@hotmail.com>
2022-02-11 10:27:02 +0100

    Some minor refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Dictator } from './dictators/classes/dictator';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dictators';
+  title: string = "Dictators";
+  dictatorList = Dictator;
 }
--- src/app/dictators/dictators.component.ts
+++ src/app/dictators/dictators.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Dictator } from './classes/dictator';
 
 @Component({
@@ -9,26 +9,15 @@ import { Dictator } from './classes/dictator';
 
 export class DictatorsComponent implements OnInit {
 
-  title: string = "Dictators";
-  dictators = Dictator;
-
-  //dictators: Dictator[] = [];
+  @Input()
+  dictators: typeof Dictator = [];
   
-  deleteDictator(id: number){
-    
-  }
-
   constructor() { }
+  
+  ngOnInit(): void { }
 
-  ngOnInit(): void {
-    /*
-    this.dictators[0].id = 1;
-    this.dictators[0].firstName = 'hans';
-    this.dictators[0].lastName = 'bob';
-    this.dictators[0].birthYear = 1191;
-    this.dictators[0].deathYear = 1112;
-    this.dictators[0].description = 'ohoy';
-    */
+  deleteDictator(id: number){
+    console.log(id);
   }
 
 }
